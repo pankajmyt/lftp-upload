7071f5924838c48f19ebd4e46da1a3491db8654f
Pankaj Myt <pankaj@myyogateacher.com>
2026-01-20 18:05:25 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ lftp ${INPUT_HOST} -u ${INPUT_USERNAME},${INPUT_PASSWORD} -e "
   set ftp:ssl-force $INPUT_FORCESSL; 
   set sftp:auto-confirm yes;
   set ssl:verify-certificate $INPUT_FORCESSL; 
-  mirror -v -P $INPUT_PCONN -R -n -x ^\.git/$ $INPUT_EXTRAFLAGS $INPUT_LOCALDIR $INPUT_REMOTEDIR;
+  mirror -v -P $INPUT_PCONN -R -x ^\.git/$ $INPUT_EXTRAFLAGS $INPUT_LOCALDIR $INPUT_REMOTEDIR;
   quit
 "
